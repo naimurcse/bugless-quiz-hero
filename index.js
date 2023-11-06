75f3f1211186fe2a17e3808fed8c4616e7563455
naimurcse <2015naim@gmail.com>
2023-11-06 16:11:23 +0600

    Question display issue is solved

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ let quizContainer = document.querySelector("#quizContainer");
 let answersContainer = document.querySelector("#answersContainer");
 let displayResult = document.querySelector("#displayResult");
 
-console.log(quizContainer);
-
 // EventListener for quiz start button
 startQuiz.addEventListener("click", () => {
    let countDown = document.querySelector("#countDownContainer");
@@ -69,7 +67,7 @@ const displayQuiz = (data) => {
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
